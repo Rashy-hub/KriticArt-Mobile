62e3459ab1786701503747854537636544d5270f
rochels <rochels2@gmail.com>
2022-07-08 09:53:59 +0200

    Amélioration des couleurs du screen comptePage

--- app/components/Bouton.js
+++ app/components/Bouton.js
@@ -1,10 +1,25 @@
-import { Pressable, Text } from "react-native"
+import { Pressable, StyleSheet, Text } from "react-native"
 
 export default Bouton = ({ title, onPress }) => {
     return <Pressable onPress={onPress} style={({ pressed }) => [
         {
             backgroundColor: pressed
-                ? 'rgb(210, 230, 255)'
-                : 'white'
-        }]}><Text>{title}</Text></Pressable>
+                ? 'lightsteelblue'
+                : 'blue'
+        }, styles.button,]}><Text style={styles.buttonText}>{title}</Text></Pressable>
 }
+
+const styles = StyleSheet.create({
+    button: {
+        marginTop: 16,
+        height: 40,
+        width: '70%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonText: {
+        padding: 8,
+        fontSize: 16,
+        color: 'antiquewhite',
+    },
+});
--- app/screens/comptePage.js
+++ app/screens/comptePage.js
@@ -1,5 +1,5 @@
 import { AppTitle } from "../components/AppTitle"
-import { StatusBar, Text, View, Button, TextInput } from 'react-native';
+import { StatusBar, Text, View, Button, TextInput, StyleSheet } from 'react-native';
 import { useAtom } from 'jotai'
 import { themeAtom } from "../atom";
 import themes from '../styles/theme'
@@ -8,10 +8,18 @@ import Bouton from "../components/Bouton";
 export const ComptePage = () => {
     const [theme, updateTheme] = useAtom(themeAtom)
     return (
-        <View style={{ flex: 1 }}>
+        <View style={styles.container}>
             <AppTitle />
-            <Text>Gestion du compte</Text>
-            <Bouton title='Press me' onPress={() => { updateTheme(theme === themes.Dark ? themes.Light : themes.Dark) }} />
+            <Text>Gestion de votre compte</Text>
+            <Bouton style={styles.myButtons} title='Changer de thème (Dark <=> Light)'
+                onPress={() => { updateTheme(theme === themes.Dark ? themes.Light : themes.Dark) }} />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+    },
+})
